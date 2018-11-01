387050176978b8c6740ba4f971a6a8ea478aa087
RahulRai05 <rahulrai0501@gmail.com>
2018-11-01 16:51:46 +0800

    Added Junit Runner class

--- SIACucumberTest/src/main/java/sia/cucumber/main/test/SSTestRunner.java
+++ SIACucumberTest/src/main/java/sia/cucumber/main/test/SSTestRunner.java
@@ -1,14 +1,26 @@
 package sia.cucumber.main.test;
 
+import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Feature", glue = {"sia.cucumber.test"}, dryRun = false, monochrome = true,
+@CucumberOptions(features = "Feature", glue = "sia.cucumber.test", dryRun = false, monochrome = true,
 		plugin={"pretty","html:TestReport/HTML","json:TestReport/JSON/JsonReport.json"}, strict = false,
 		tags = {"~@negative"})
 public class SSTestRunner {
+	
+	public static void main(String[] args) {
+		try{
+			 JUnitCore.main("sia.cucumber.main.test.SSTestRunner");    
+			}catch(Exception e){
+				System.out.println("Excpetion occured while invoking Junit");
+			}
+
+
+	}
+
 
 }
